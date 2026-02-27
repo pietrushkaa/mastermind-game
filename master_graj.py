c8b7730e419ecd8084c2f98c57aa43ce608c25dc
Aga <agnieszkasarkowicz01@gmail.com>
2026-02-27 14:39:58 +0100

    Translating master_graj.py into english

--- master_graj.py
+++ master_graj.py
@@ -1,6 +1,6 @@
 from kivy.config import Config
 
-# Wyłączenie pełnego ekranu i ustawienie stałych rozmiarów okna
+# Disabling fullscreen and setting fixed window sizes
 Config.set("graphics", "fullscreen", "0")
 Config.set("graphics", "width", "1200")
 Config.set("graphics", "height", "1000")
@@ -21,21 +21,21 @@ from kivy.graphics import Line
 from sedzia import sprawdz
 import random
 
-# Zmienne globalne
+# Global variables
 spis_zasad = """
-Zasady gry:
-1. Poniżej wpisz długość szyfru, jaki chcesz zgadywać i liczbę kolorów.
-2. Masz 12 tur, żeby odgadnąć szyfr złożony z kolorów. Kolory mogą się powtarzać.
-3. W celu wpisania szyfru, kliknij w kolor, a następnie w pole w rzędzie odpowiadającej danej turze.
-4. Po wybraniu wszystkich kolorów kliknij 'sprawdź'. Jeśli szyfr nie jest poprawny,
-po prawej pokaże się, ile jest właściwych kolorów na właściwym miejscu, a ile właściwych kolorów na niewłaściwym miejscu.
-Powodzenia!
+Game rules:
+1. Below, enter the length of the code you want to guess and the number of colors.
+2. You have 12 turns to guess the color code. Colors can repeat.
+3. To enter the code, click a color, then click a slot in the row corresponding to the given turn.
+4. After choosing all colors, click 'check'. If the code is incorrect,
+the right side will show how many right colors are in the right place, and how many right colors are in the wrong place.
+Good luck!
 """
 tekst_legendy = """
-Legenda:
+Legend:
 x | y
-x - liczba pionków o właściwym kolorze i na właściwym miejscu
-y - liczba pionków o właściwym kolorze, ale na niewłaściwym miejscu
+x - number of pegs of the right color and in the right place
+y - number of pegs of the right color, but in the wrong place
 """
 lista_kolorow = [
     "#00FFFF",
@@ -48,31 +48,31 @@ lista_kolorow = [
     "#008080",
     "#964b00",
     "#a7ff6a",
-]  # lista możliwych do użycia w grze kolorów w kodzie Hex
+]  # list of colors available to use in the game in Hex code
 liczba_kolorow = 0
 dlugosc_sekwencji = 0
-aktualny_kolor = None  # kolor wybrany ostatnio przez gracza podczas rozgrywki
-zapytanie = []  # lista kodująca pojedyncze zapytanie gracza
-odpowiedz = []  # lista kodująca sekwencję ukrytą
-tura = 0  # aktualna tura gry, indeksowana od 0
+aktualny_kolor = None  # color recently chosen by the player during the game
+zapytanie = []  # list encoding a single guess of the player
+odpowiedz = []  # list encoding the hidden sequence
+tura = 0  # current game turn, indexed from 0
 
 
 class OknoGry(FloatLayout):
     def __init__(self, **kwargs):
         """
-        Inicjalizator klasy odpowiadającej za główne okno gry
-        Tworzy przyciski poddania i sprawdzania sekwencji,
-        napisy i układ graficzny bazujący na FloatLayout
+        Initializer of the class responsible for the main game window
+        Creates surrender and check sequence buttons,
+        labels, and the graphical layout based on FloatLayout
         """
         global tekst_legendy
-        # wywołanie inicjalizatora klasy nadrzędnej - FloatLayout
+        # calling the initializer of the parent class - FloatLayout
         super().__init__(**kwargs)
         Window.clearcolor = get_color_from_hex("#585858")
         self.spacing = 100
 
-        # dodanie przycisku poddania się
+        # adding the surrender button
         self.poddaj_sie = Button(
-            text="PODDAJ SIĘ",
+            text="GIVE UP",
             font_size=18,
             size_hint=(0.12, 0.09),
             pos_hint={"top": 0.97, "x": 0.03},
@@ -82,9 +82,9 @@ class OknoGry(FloatLayout):
         )
         self.add_widget(self.poddaj_sie)
 
-        # dodanie przycisku sprawdzania
+        # adding the check button
         self.sprawdz = Button(
-            text="SPRAWDŹ!",
+            text="CHECK!",
             font_size=18,
             size_hint=(0.12, 0.09),
             pos_hint={"right": 0.97, "center_y": 0.5},
@@ -94,7 +94,7 @@ class OknoGry(FloatLayout):
         )
         self.add_widget(self.sprawdz)
 
-        # dodanie napisów "Mastermind" i legendy
+        # adding "Mastermind" label and legend
         self.mastermind_napis = Label(
             text="MASTERMIND",
             pos_hint={"center_x": 0.5, "top": 1},
@@ -115,14 +115,14 @@ class OknoGry(FloatLayout):
 
     def wybierz_kolor(self, instance):
         """
-        Zmienio zmienną globalną aktualny_kolor na kolor właśnie klkniętego przycisku koloru
-        i otacza ramką obecnie aktywny przycisk koloru, jednocześnie usuwając ramkę z poprzedniego aktywnego
+        Changes the global variable aktualny_kolor to the color of the just clicked color button
+        and outlines the currently active color button, while removing the outline from the previous active one
         """
         global aktualny_kolor
-        # usuwanie poprzedniej ramki
+        # removing the previous outline
         if aktualny_kolor != None:
             self.kolory.children[liczba_kolorow - aktualny_kolor].canvas.before.clear()
-        # tworzenie ramki na aktualnym przycisku koloru
+        # creating an outline on the current color button
         with instance.canvas.before:
             Line(
                 rectangle=(instance.x, instance.y, instance.width, instance.height),
@@ -132,9 +132,9 @@ class OknoGry(FloatLayout):
 
     def zmien_kolor(self, instance):
         """
-        Zmienia kolor danego pola na planszy na aktualny kolor
-        i aktualizuje listę globalną zapytanie pod
-        numerem tego pola na aktualny kolor
+        Changes the color of a given slot on the board to the current color
+        and updates the global list zapytanie at the
+        index of this slot to the current color
         """
         global aktualny_kolor, zapytanie
         if aktualny_kolor != None:
@@ -146,16 +146,16 @@ class OknoGry(FloatLayout):
             zapytanie[id_pola] = aktualny_kolor
 
     def wylacz_gre(self, instance=None):
-        """Wyłącza całą aplikację"""
+        """Closes the entire application"""
         App.get_running_app().stop()
 
     def okienko_przegranej(self, instance=None):
         """
-        Otwiera okienko pop_up informujące o przegranej
-        i ujawniające sekwencję ukrytą
+        Opens a popup window informing about the loss
+        and revealing the hidden sequence
         """
         global odpowiedz, liczba_kolorow
-        # stworzenie graficznej reprezentacji sekwencji ukrytej
+        # creating a graphical representation of the hidden sequence
         odp_graficznie = GridLayout(
             cols=dlugosc_sekwencji,
             padding=30,
@@ -169,15 +169,15 @@ class OknoGry(FloatLayout):
                     background_color=get_color_from_hex(lista_kolorow[kolor - 1]),
                 )
             )
-        # tworzenie układu graficznego okienka
+        # creating the window's graphical layout
         layout_koniec = BoxLayout(orientation="vertical", spacing=50, padding=20)
         layout_koniec.add_widget(
-            Label(text="Niestety, przegrałeś! Prawidłowa sekwencja to:", font_size=17)
+            Label(text="Unfortunately, you lost! The correct sequence is:", font_size=17)
         )
         layout_koniec.add_widget(odp_graficznie)
-        layout_koniec.add_widget(Button(text="Wyłącz grę", on_press=self.wylacz_gre))
-        przegrana = Popup(  # okienko przegranej
-            title="Przegrana!",
+        layout_koniec.add_widget(Button(text="Close game", on_press=self.wylacz_gre))
+        przegrana = Popup(  # loss popup
+            title="Game Over!",
             content=layout_koniec,
             size_hint=(None, None),
             size=(500, 300),
@@ -187,16 +187,16 @@ class OknoGry(FloatLayout):
 
     def sprawdzaj_zapytanie(self, instance):
         """
-        Sprawdza zapytanie, wyświetla komunikat o wygranej lub przegranej
-        Aktualizuje zmienną tura i aktywuje kolejny rząd na planszy
+        Checks the guess, displays a win or loss message
+        Updates the tura variable and activates the next row on the board
         """
         global tura, zapytanie, odpowiedz, liczba_kolorow
-        # przypadek, gdy zapytanie nie jest całkowicie zapełnione
+        # case when the guess is not completely filled
         if not all(isinstance(x, int) for x in zapytanie):
             niepelne_zapytanie = Popup(
-                title="Niepełne zapytanie",
+                title="Incomplete guess",
                 content=Label(
-                    text="Żeby sprawdzić zapytanie, wypełnij najpierw wszystkie puste pola!",
+                    text="To check the guess, fill in all empty slots first!",
                     font_size=17,
                 ),
                 size_hint=(None, None),
@@ -205,21 +205,21 @@ class OknoGry(FloatLayout):
             )
             niepelne_zapytanie.open()
             return
-        # sprawdzanie zapytania przy użyciu funkcji z modułu sedzia.py
+        # checking the guess using the function from the sedzia.py module
         wlasciwe_miejsca, wlasciwe_kolory = sprawdz(
             liczba_kolorow, odpowiedz, zapytanie
         )
-        # przypadek wygranej gracza
+        # case of player's victory
         if wlasciwe_miejsca == len(odpowiedz):
             tekst_wygranej = (
-                "Brawo! Udało ci się odgadnąć sekwencję!\n"
-                + "Sekwencja odgadnięta w "
+                "Congratulations! You successfully guessed the sequence!\n"
+                + "Sequence guessed in "
                 + str(tura + 1)
-                + " turach.\n"
-                + "Gra zamknie się automatycznie."
+                + " turns.\n"
+                + "The game will close automatically."
             )
             wygrana = Popup(
-                title="Wygrana!",
+                title="Victory!",
                 content=Label(text=tekst_wygranej, font_size=17),
                 size_hint=(None, None),
                 size=(600, 200),
@@ -228,14 +228,14 @@ class OknoGry(FloatLayout):
             wygrana.open()
             Clock.schedule_once(self.wylacz_gre, 4)
             return
-        # przypadek przegranej gracza z powodu rozegrania ostatniej tury
+        # case of player's loss due to playing the last turn
         if tura == 11:
             self.okienko_przegranej()
             return
-        # przypadek, gdy zapytanie nie było prawidłowe, ale nie nastąpiła przegrana
+        # case when the guess was incorrect, but the game is not lost
         for i in range(
             1, dlugosc_sekwencji + 1
-        ):  # wyłączenie przycisków aktywnych w rzędzie o numerze poprzedniej tury
+        ):  # disabling active buttons in the row of the previous turn
             self.plansza.children[(dlugosc_sekwencji + 1) * tura + i].disabled = True
         self.plansza.children[(dlugosc_sekwencji + 1) * tura].text = (
             str(wlasciwe_miejsca) + " | " + str(wlasciwe_kolory)
@@ -243,15 +243,15 @@ class OknoGry(FloatLayout):
         tura += 1
         for i in range(
             1, dlugosc_sekwencji + 1
-        ):  # włączenie przycisków aktywnych w rzędzie o numerze teraźniejsze tury
+        ):  # enabling active buttons in the row of the current turn
             self.plansza.children[(dlugosc_sekwencji + 1) * tura + i].disabled = False
         zapytanie = [None for i in range(dlugosc_sekwencji)]
 
     def stworz_kod(self):
         """
-        Aktualizuje zmienne globalne:
-        zapytanie - listą o długości sekwencji wypełnioną None
-        odpowiedz - losowo wybraną spośród dostępnych kolorów sekwencją ukrytą
+        Updates global variables:
+        zapytanie - with a list of the sequence length filled with None
+        odpowiedz - with a randomly chosen hidden sequence from available colors
         """
         global zapytanie, odpowiedz
         zapytanie = [None for i in range(dlugosc_sekwencji)]
@@ -259,9 +259,9 @@ class OknoGry(FloatLayout):
 
     def dodaj_plansze(self):
         """
-        Dodaje przyciski kolorów i planszy do głównego okna gry
+        Adds color and board buttons to the main game window
         """
-        # dodanie planszy do gry
+        # adding the game board
         global tura, lista_kolorow
         self.plansza = GridLayout(
             cols=dlugosc_sekwencji + 1,
@@ -285,7 +285,7 @@ class OknoGry(FloatLayout):
             )
         self.add_widget(self.plansza)
 
-        # dodanie przycisków kolorów
+        # adding color buttons
         self.kolory = BoxLayout(
             orientation="horizontal",
             spacing=15,
@@ -306,23 +306,23 @@ class OknoGry(FloatLayout):
 class OknoStartowe(Popup):
     def __init__(self, okno_glowne, **kwargs):
         """
-        Inicjalizator klasy odpowiadającej za okno startowe gry
-        Tworzy napisy: tytuł i spis zasad, pola tekstowe do
-        wpisywania liczby kolorów i długości sekwencji
-        przycisk startu i układ graficzny bazujący na BoxLayout
+        Initializer of the class responsible for the start window of the game
+        Creates labels: title and rules list, text fields for
+        entering the number of colors and sequence length,
+        start button, and the graphical layout based on BoxLayout
         """
-        # wywołanie inicjalizatora klasy nadrzędnej - Popup
+        # calling the initializer of the parent class - Popup
         super().__init__(**kwargs)
         self.okno_glowne = okno_glowne
         layout_start = BoxLayout(orientation="vertical", spacing=30, padding=90)
 
-        # dodanie tytułu gry
+        # adding the game title
         tytul = Label(
-            text="Zagraj w Mastermind!", font_size=50, halign="center", bold=True
+            text="Play Mastermind!", font_size=50, halign="center", bold=True
         )
         layout_start.add_widget(tytul)
 
-        # dodanie spisu zasad gry
+        # adding the game rules list
         reguly = Label(
             text=spis_zasad,
             font_size=17,
@@ -335,29 +335,29 @@ class OknoStartowe(Popup):
         )
         layout_start.add_widget(reguly)
 
-        # dodanie pola do wpisywania liczby kolorów
+        # adding the field for entering the number of colors
         self.ile_kolorow = TextInput(
             size_hint=(0.1, None),
             height=30,
             pos_hint={"center_x": 0.5, "top": 1},
         )
         layout_start.add_widget(
-            Label(text="Wpisz liczbę kolorów (od 2 do 10):", font_size=17)
+            Label(text="Enter the number of colors (from 2 to 10):", font_size=17)
         )
         layout_start.add_widget(self.ile_kolorow)
 
-        # dodanie pola do wpisywania długości sekwencji
+        # adding the field for entering the sequence length
         self.jaka_dlugosc = TextInput(
             size_hint=(0.1, None),
             height=30,
             pos_hint={"center_x": 0.5, "top": 1},
         )
         layout_start.add_widget(
-            Label(text="Wpisz długość sekwencji (od 1 do 10):", font_size=17)
+            Label(text="Enter the sequence length (from 1 to 10):", font_size=17)
         )
         layout_start.add_widget(self.jaka_dlugosc)
 
-        # dodanie przycisku startowego
+        # adding the start button
         przycisk_start = Button(
             text="START",
             font_size=17,
@@ -368,35 +368,35 @@ class OknoStartowe(Popup):
         )
         layout_start.add_widget(przycisk_start)
 
-        self.title = "Ekran startowy"
+        self.title = "Start screen"
         self.content = layout_start
         self.auto_dismiss = False
 
     def przechwyc_tekst(self, instance):
         """
-        Uzupełnia zmienne globalne liczba_kolorow i dlugosc_sekwencji
-        tekstem wpisanym w polach tekstowych
-        Tworzy w głównym oknie poprzez odpowiednie funkcje z klasy OknoGry
-        planszę i przyciski kolorów oraz zamyka okno startowe
-        W razie pustego pola lub gdy dane nie są liczbą
-        aktywuje okienko z błędem
+        Fills the global variables liczba_kolorow and dlugosc_sekwencji
+        with the text entered in the text fields
+        Creates the board and color buttons in the main window via appropriate functions from the OknoGry class
+        and closes the start window
+        If a field is empty or the data is not a number,
+        it triggers an error popup
         """
         global liczba_kolorow, dlugosc_sekwencji
         k = self.ile_kolorow.text
         dl = self.jaka_dlugosc.text
-        # tworzenie okienka z błędem
+        # creating the error popup
         blad = Popup(
-            title="Błąd!",
+            title="Error!",
             content=Label(
-                text="Wpisz poprawne wartości, które są liczbami z podanego przedziału!",
+                text="Enter valid values, which are numbers from the given range!",
                 font_size=17,
             ),
             size_hint=(None, None),
             size=(630, 250),
             auto_dismiss=True,
         )
-        # próba stworzenia planszy i przycisków kolorów,
-        # w razie niepoprawnych danych otworzenie okienka z błędem
+        # attempting to create the board and color buttons,
+        # in case of incorrect data, opening the error popup
         try:
             liczba_kolorow = int(k)
             dlugosc_sekwencji = int(dl)
@@ -413,8 +413,8 @@ class OknoStartowe(Popup):
 class Gra(App):
     def build(self):
         """
-        Tworzy i zwraca interfejs graficzny okna głównego gry
-        Otwiera okno startowe tuż po otwarciu aplikacji
+        Creates and returns the graphical interface of the main game window
+        Opens the start window right after opening the application
         """
         self.okno_glowne = OknoGry()
         self.okno_startowe = OknoStartowe(okno_glowne=self.okno_glowne)
@@ -422,7 +422,7 @@ class Gra(App):
         return self.okno_glowne
 
     def otworz_okno_startowe(self, instance):
-        """Otwiera okno startowe gry"""
+        """Opens the start window of the game"""
         self.okno_startowe.open()
 
 
